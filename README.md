4c7fdc10a98724c885818dee83f59f06133d26d8
dewasatriawan <dewa@bsa.id>
2023-10-27 15:56:56 +0800

    add segementation chacter

--- README.md
+++ README.md
@@ -10,16 +10,30 @@ With NPM version : 6.14.16
 
 2. Install using ```npm install```
 
+opencv-build-npm
 
 #use command 
 
 #Convert Image
 node .\main.js biner -i img\test\lena.png -o img\test_out\lena-biner.jpg
+node .\main.js biner -i img\test\meter.png -o img\test_out\meter-biner.jpg
+
 node .\main.js gray -i img\test\lena.png -o img\test_out\lena-gray.jpg
 
+node .\main.js prepwater -i img\test\meter.png -o img\test_out\meter-out.jpg
+
+#segment image
+
+node .\main.js segment -i img\test\angka.png -o img\test_out\
+
 #extract Feature
-node .\main.js extract -i dataset-img\mnist\ -o dataset-feature\mnist.xlsx
+node .\main.js extract -i dataset-img\mnist\train -o dataset-feature\mnist.xlsx
+node .\main.js extract -i dataset-img\mnist\test -o dataset-feature\mnist-test.xlsx
 
 #training
 
 node .\main.js training -i dataset-feature\mnist.xlsx -o model\mnist.json
+
+#testing
+
+node .\main.js testing -m model\mnist.json -i dataset-feature\mnist-test.xlsx
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import yargs from 'yargs';
 import { NeuralNetwork } from './lib/deep.mjs';
-import { PreprocessingImage } from './lib/preprocessing.mjs';
+import { normalizeArray2D, invertBinaryArray2D, textLineSegmentation, charColumnSegmentation, getArrayDimensions, PreprocessingImage } from './lib/preprocessing.mjs';
 import { extractFeaturesAndSaveToExcel, readExcel } from './lib/extract.mjs';
+// import { TextSegmentation } from './lib/textSegement.mjs';
 
 
 const argv = yargs
@@ -30,6 +31,36 @@ const argv = yargs
         output: {
             alias: 'o',
             describe: 'Lokasi hasil konversi gambar',
+            demandOption: true,
+            type: 'string',
+        },
+    })
+
+    .command('prepwater', 'Proses preprocessing water meter', {
+        input: {
+            alias: 'i',
+            describe: 'Path lokasi gambar yang akan dipreprocessing',
+            demandOption: true,
+            type: 'string',
+        },
+        output: {
+            alias: 'o',
+            describe: 'Folder lokasi hasil preprocessing',
+            demandOption: true,
+            type: 'string',
+        },
+    })
+
+    .command('segment', 'Proses segmentasi gambar', {
+        input: {
+            alias: 'i',
+            describe: 'Path lokasi gambar yang akan disegmentasi',
+            demandOption: true,
+            type: 'string',
+        },
+        output: {
+            alias: 'o',
+            describe: 'Folder lokasi file segmentasi',
             demandOption: true,
             type: 'string',
         },
@@ -64,6 +95,22 @@ const argv = yargs
             type: 'string',
         },
     })
+
+    .command('testing', 'Testing model', {
+        model: {
+            alias: 'm',
+            describe: 'Input path file model json',
+            demandOption: true,
+            type: 'string',
+        },
+        input: {
+            alias: 'i',
+            describe: 'Lokasi data test',
+            demandOption: true,
+            type: 'string',
+        },
+    })
+
     .help()
     .argv;
 
@@ -107,6 +154,67 @@ if (method === 'gray') {
         .then(() => console.log('Feature extraction completed.'))
         .catch(error => console.error('Error:', error));
 
+} else if (method === 'segment') {
+    const inputPath = argv.input;
+    const outputPath = argv.output;
+    const threshold = 128; // Set your desired threshold value here
+
+    if (typeof threshold !== 'number') {
+        console.error('Threshold must be a valid number.');
+    } else {
+        const preprocessing = new PreprocessingImage(inputPath);
+        (async () => {
+            try {
+                await preprocessing.loadImage();
+                await preprocessing.toBinary(threshold);
+                // await preprocessing.saveImage(outputPath, 1);
+                console.log('Image converted to binary successfully.');
+                var data = await preprocessing.imageToArray();
+                data = normalizeArray2D(data);
+                data = invertBinaryArray2D(data);
+                console.log("data length: ", getArrayDimensions(data))
+                var lineSegment = textLineSegmentation(data);
+                var character = charColumnSegmentation(lineSegment[0]);
+
+                for (var i = 0; i < character.length; i++) {
+                    var dimension = getArrayDimensions(character[i]);
+                    await preprocessing.arrayBinerToImage(character[i], dimension.width, dimension.height)
+                    await preprocessing.saveImage(outputPath + "-" + i + ".jpg");
+                }
+                // console.log("character Segment: ", character[0]);
+
+                // var dimension = getArrayDimensions(character[2]);
+                // await preprocessing.arrayBinerToImage(character[2], dimension.width, dimension.height)
+                // await preprocessing.saveImage(outputPath);
+                // console.log("segment: ", getArrayDimensions(lineSegment[0]));
+
+            } catch (error) {
+                console.error('Error processing the image:', error);
+            }
+        })();
+    }
+
+} else if (method === 'prepwater') {
+    const inputPath = argv.input;
+    const outputPath = argv.output;
+    const threshold = 128; // Set your desired threshold value here
+
+    if (typeof threshold !== 'number') {
+        console.error('Threshold must be a valid number.');
+    } else {
+        const preprocessing = new PreprocessingImage(inputPath);
+        (async () => {
+            try {
+                await preprocessing.loadImage();
+                await preprocessing.prepWaterMeter(threshold);
+                await preprocessing.saveImage(outputPath, 1);
+                console.log('Image converted to binary successfully.');
+            } catch (error) {
+                console.error('Error processing the image:', error);
+            }
+        })();
+    }
+
 } else if (method === 'training') {
     const inputPath = argv.input;
     const outputPath = argv.output; // Lokasi penyimpanan model
@@ -116,8 +224,8 @@ if (method === 'gray') {
 
         const inputNodes = features[0].length;
         const outputNodes = numberOfClass;
-        const epochSize = 100;
-        const learningRate = 0.1;
+        const epochSize = 200;
+        const learningRate = 0.01;
 
         console.log('Panjang Fitur Vektor:', inputNodes);
         console.log('Jumlah Kelas:', numberOfClass);
@@ -132,4 +240,53 @@ if (method === 'gray') {
         })();
 
     })
+} else if (method === 'testing') {
+    const inputPath = argv.input;
+    const modelPath = argv.model;
+
+    const nn = new NeuralNetwork(0, 0);
+
+    readExcel(inputPath).then(data => {
+        const { numberOfClass, labels, features } = data;
+
+        console.log("labels: ", labels);
+
+        // const inputNodes = features[0].length;
+        // const outputNodes = numberOfClass;
+        // const epochSize = 100;
+        // const learningRate = 0.1;
+
+        // console.log('Panjang Fitur Vektor:', inputNodes);
+        // console.log('Jumlah Kelas:', numberOfClass);
+
+        // const nn = new NeuralNetwork(inputNodes, outputNodes);
+
+        // nn.train(features, labels, epochSize, learningRate);
+
+        (async () => {
+            await nn.loadModel(modelPath);
+            const score = nn.test(features, labels);
+            console.log("Score Test : ", score);
+        })();
+
+    })
+
+
+
+    // readExcel(inputPath).then(data => {
+    //     const {_, actualLabels, features} = data;
+
+    //     console.log("actualLabels: ", actualLabels);
+    //     // console.log("features: ", features);
+
+    //     // (async () => {
+    //     //     await nn.loadModel(modelPath);
+    //     //     const score = nn.test(features, actualLabels);
+    //     //     console.log("Score Test : ", score);
+    //     // })();
+
+
+
+    // })
+
 }
